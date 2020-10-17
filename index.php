d17c77bedc792a0c5d7e5c936d7ec16fb664e9c9
Rohianon <rohianon48@gmail.com>
2020-10-17 09:49:40 +0300

    New Code added

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php require_once("config.php") ?>
+<?php require_once( ROOT_PATH ."/includes/header.php") ?>
+<?php inlcude(ROOT_PATH ."/includes/nav.php") ?>
+
+<div class="content">
+	<div class="bio-brief">
+		<?php echo "<img src='./static/img/me.jpg' width='100%' height='100%'>" ; ?>
+	</div>
+	<div class="bio">
+			<p id="quote"></p>
+			<div id="about">
+				<div class="bio-span">About ROHI</div>
+				<p>My name is Rohi Anon Ogula, AKA RAO. I am an apt quirk with lots of love for computers</p>
+				<p>Currently, I am 21 years old eager to explore the whole lot of computing world especially data science and web-development</p>
+				<p>I study at Jomo Kenyatta University of Agriculture and Technology, pursuing a degree in Statistics</p>
+				<p>County of Residence is Busia (Kenya)</p>
+				<p>My area of specialization is kinda ... Actually a better term to describe me is that I am a generalist. Howerver, I love Math more than anything else.</p>
+				<p>The best language tool I often use is Python, followed by R. Web tools have been a nightmare for me for a long time. After understanding that models ought to be deployed I had to learn it the hard way.</p>
+				<p>I tend to love opportunities and take them no matter the cost. Recently, after finishing up with my Machine Learning Track at coursera, I decided to get deep into the world of web. A fried of mine called Siranjofu Wachiye motivated me to with the words <em>MY FUTURE LIES IN MY WEB BROWSER</em></p>
+				<p>This other <em>MY NEXT MOVE/PROJECT IS THE BEST OF ALL</em> gave me a jumpstart</p>
+				<p>With all this said and done, here comes the first of it's kind</p>
+				<p>Django is soon coming</p>
+
+				<p>If you'd love us to share idea, hit the links below. </p>
+
+			</div>
+			<div id="skills">
+				
+			</div>
+	</div>
+</div>
